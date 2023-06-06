fb8b8c16e76a2710546e5ff4966e25d3a7807307
Jailson Dias <jaison.diasc@gmail.com>
2023-06-05 22:40:02 -0300

    feature: Add anotations to enum and required item on models

--- example/gin/models/models.go
+++ example/gin/models/models.go
@@ -3,20 +3,21 @@ package models
 import "time"
 
 type Product struct {
-	Id         uint64    `json:"id" example:"5"`
-	Name       string    `json:"name" example:"Product name"`
+	Id         uint64    `json:"id" binding:"required" example:"5"`
+	Name       string    `json:"name" binding:"required" example:"Product name"`
 	MerchantId uint64    `json:"merchant_id"`
 	CategoryId *uint64   `json:"category_id,omitempty"`
 	Tags       []uint64  `json:"tags" example:"1"`
 	Images     []string  `json:"image_ids" example:"image_id"`
 	Sizes      []Sizes   `json:"sizes"`
 	SaleDate   time.Time `json:"sale_date"`
-	EndDate    time.Time `json:"end_date"`
+	EndDate    time.Time `json:"end_date" binding:"required"`
 	Exclude    string    `json:"-"`
+	Type       string    `json:"type" binding:"required" example:"Physical" enum:"Physical|Online"`
 }
 
 type Sizes struct {
-	Size string `json:"size" example:"size"`
+	Size string `json:"size" example:"size" binding:"required"`
 }
 
 type ProductPost struct {
